6efae9c25ac475cb7f67f47ef1d47456b49bbecb
LukaszJaniak <101112200+LukaszJaniak@users.noreply.github.com>
2023-06-07 01:21:44 +0200

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,3 +2,6 @@ source /opt/ros/humble/setup.bash
 sudo apt install ros-humble-navigation2
 sudo apt install ros-humble-nav2-bringup
 sudo apt install ros-humble-turtlebot3-gazebo
+colcon build
+source /opt/ros/humble/setup.bash
+source install/setup.bash
